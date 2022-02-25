5e6c0ea2833753b37d97343f407ccb19273b8d48
shivambang <shivam.bang@gmail.com>
2022-02-24 23:45:21 -0500

    formula correction

--- multi-crop/script.py
+++ multi-crop/script.py
@@ -3,14 +3,17 @@ import cv2
 import matplotlib.pyplot as plt
 
 r, c = [int(i) for i in sys.argv[1:]]
-img = cv2.imread('1.jpg')
+img = cv2.imread('2.jpg')
 height, width, channels = img.shape
 b = width/c
 h = height/r
+# print(b, h)
 for i in range(r):
     for j in range(c):
-        y = int(j*h)
-        x = int(i*b)
-        crop_img = img[x+4:x+int(h)-32, y+12:y+int(h)-8] # Crop from {x, y, w, h } => {0, 0, 300, 400}
-
-        cv2.imwrite(f"images/{4}-{j+1}-{i}.jpg", crop_img)
+        y = int(j*b)
+        x = int(i*h)
+        crop_img = img[x+64:int(x+h)-16, y+32:int(y+b)-32] # Crop from {x, y, w, h } => {0, 0, 300, 400}
+        # print(x, x+int(h), y, y+int(b))
+        cv2.imshow("cropped", crop_img)
+        cv2.waitKey(0)
+        # cv2.imwrite(f"images/{4}-{2*(j+1)}-{i}.jpg", crop_img)
